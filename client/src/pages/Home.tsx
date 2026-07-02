08aca36661d63b5f30d97bbb209e87ddc54bae1c
Sabea Hassan <hsabae497@gmail.com>
2026-07-02 11:27:22 -0400

    frontend inputs added- tested & working

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,20 +1,71 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonButton, IonToolbar, IonList, IonItem, IonInput, IonText } from '@ionic/react';
+import {useState} from 'react'; 
+
 
 const Home: React.FC = () => {
+  const[roomCode, setRoomCode]=useState('');
+  const[username, setUsername]=useState(''); 
+  const[answer, setAnswer]=useState('');
+  const[recentAnswer, setrecentAnswer]= useState(''); 
+  
+    
+    const handleSubmit=() => {
+      if (!answer.trim())return;  //doesnt allow empty input
+      setrecentAnswer(answer);
+      setAnswer('');
+    }; 
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonTitle>Celebrity Name Chain</IonTitle>
         </IonToolbar>
+
       </IonHeader>
       <IonContent fullscreen>
-        <p>Hello, world!</p>
+            <IonList>
+              <IonItem>
+                 <IonInput 
+                 label="Room Code"
+                 value={roomCode}
+                 onIonInput={(e:any)=> setRoomCode(e.detail.value!)}
+                 /> 
+
+               </IonItem>
+                <IonItem>
+                  <IonInput label="Username"
+                  value={username}
+                  onIonInput={(e:any)=> setUsername(e.detail.value!)}
+                 />
+
+               </IonItem>
+               <IonItem>
+               <IonInput
+                 label="Answer" 
+                 placeholder="Please enter celebrity name guess"
+                 value={answer}
+                 onIonInput={(e:any)=> setAnswer(e.detail.value!)}
+                />
+                 </IonItem>
+
+               <IonButton expand="block" onClick={handleSubmit}>
+                Sumbit</IonButton>
+
+                {recentAnswer &&(
+                  <IonItem>
+                    <IonText>
+                      Most recent answer: <strong> {recentAnswer}</strong>
+                    </IonText>
+                  </IonItem>
+
+                )}
+
+           </IonList>
       </IonContent>
     </IonPage>
   );
 };
-
 export default Home;
 
 
